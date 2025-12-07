a4b586403482c74a8bd29e2873cc7dbffc786951
FatemehSeyfi <80269176+FatemehSeyfi@users.noreply.github.com>
2025-12-07 15:21:42 +0330

    Update generate_data_resume.py
    Signed-off-by: FatemehSeyfi <80269176+FatemehSeyfi@users.noreply.github.com>

--- generate_data_resume.py
+++ generate_data_resume.py
@@ -208,7 +208,7 @@ for x in range(180):
 
 # --- Resume-Capable Main ---
 if __name__ == "__main__":
-    root_dir = root_dir = "/content/drive/MyDrive/relformer_data/20cities"
+    root_dir = root_dir = "./data/20cities"
 
 
     train_path = os.path.join(root_dir, "train_data")
@@ -265,4 +265,5 @@ if __name__ == "__main__":
         mesh.lines = patch_edge.flatten()
 
         patch_extract(test_path, sat_img, gt_seg, mesh)
+
 
